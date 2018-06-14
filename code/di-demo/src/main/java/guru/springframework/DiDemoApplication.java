356a1150256de98747758674b6b3ead8c200cb73
Enok <enok.jesus@gmail.com>
2018-06-14 11:52:30 -0300

    Properties

--- code/di-demo/src/main/java/guru/springframework/DiDemoApplication.java
+++ code/di-demo/src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,9 +1,8 @@
 package guru.springframework;
 
-import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.MyController;
-import guru.springframework.controllers.PropertyInjectedController;
-import guru.springframework.controllers.SetterInjectedController;
+import guru.springframework.examplesbeans.FakeDataSource;
+import guru.springframework.examplesbeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +15,10 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser());
+
+        FakeJmsBroker fakeJmsBroker = ctx.getBean(FakeJmsBroker.class);
+        System.out.println(fakeJmsBroker.getUsername());
     }
 }
---
+++ code/di-demo/src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,54 @@
+package guru.springframework.config;
+
+import guru.springframework.examplesbeans.FakeDataSource;
+import guru.springframework.examplesbeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by enok on 14/06/2018
+ */
+@Configuration
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+//@PropertySources({
+//        @PropertySource({"classpath:datasource.properties"}),
+//        @PropertySource({"classpath:jms.properties"})
+//})
+public class PropertyConfig {
+    @Value("${guru.username}")
+    private String user;
+
+    @Value("${guru.password}")
+    private String password;
+
+    @Value("${guru.dburl}")
+    private String url;
+
+    @Value("${guru.jms.username}")
+    private String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    private String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    private String jmsUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        return fakeJmsBroker;
+    }
+}
