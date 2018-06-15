40c76454b3412d9cf00523af7d9df6b98e665d3e
Enok <enok.jesus@gmail.com>
2018-06-15 14:06:27 -0300

    Logs

--- code/di-demo/src/main/java/guru/springframework/DiDemoApplication.java
+++ code/di-demo/src/main/java/guru/springframework/DiDemoApplication.java
@@ -3,14 +3,20 @@ package guru.springframework;
 import guru.springframework.controllers.MyController;
 import guru.springframework.examplesbeans.FakeDataSource;
 import guru.springframework.examplesbeans.FakeJmsBroker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
+    private final static Logger LOG = LoggerFactory.getLogger(DiDemoApplication.class);
 
     public static void main(String[] args) {
+        LOG.info("...Starting application...");
+        LOG.info("args: {}", args);
+
         ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
         MyController controller = (MyController) ctx.getBean("myController");
@@ -20,5 +26,11 @@ public class DiDemoApplication {
 
         FakeJmsBroker fakeJmsBroker = ctx.getBean(FakeJmsBroker.class);
         System.out.println(fakeJmsBroker.getUsername());
+
+        try {
+            ctx.getBean("bla");
+        } catch (Exception e) {
+            LOG.error("Error", e);
+        }
     }
 }
