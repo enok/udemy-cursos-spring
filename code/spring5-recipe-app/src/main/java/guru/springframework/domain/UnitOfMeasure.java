fc35f0fac554c46a1b64ba7f77a580580564f1db
Enok <enok.jesus@gmail.com>
2018-06-22 16:42:57 -0300

    complete de assignment

---
+++ code/spring5-recipe-app/src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by enok on 22/06/2018
+ */
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
