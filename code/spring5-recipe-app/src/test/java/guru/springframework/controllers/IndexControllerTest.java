e734b740e99b0655d619757e8d0ae9b1f2e9daf7
Enok <enok.jesus@gmail.com>
2018-06-27 12:14:58 -0300

    circle ci

---
+++ code/spring5-recipe-app/src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by enok on 26/06/2018
+ */
+public class IndexControllerTest {
+
+}
---
+++ code/spring5-recipe-app/src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by enok on 27/06/2018
+ */
+public class UnitOfMeasureRepositoryIT {
+
+}
